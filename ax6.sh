c7471cc3ea324d26ffcf123c088c657070283a50
takayukileung <73388504+takayukileung@users.noreply.github.com>
2022-08-05 13:54:56 +0800

    Update ax6.sh

--- ax6.sh
+++ ax6.sh
@@ -44,9 +44,9 @@ sed -i 's/443/57002/g' feeds/luci/applications/luci-app-frpc/root/etc/config/frp
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
 #zip -rP Jp3895 files.zip files
-#wget -O files.zip https://raw.githubusercontent.com/takayukileung/lede/master/config/files.zip
-#unzip -P Jp3895 files.zip
-#rm files.zip
+wget -O files.zip https://raw.githubusercontent.com/takayukileung/lede/master/config/files.zip
+unzip -P Jp3895 files.zip
+rm files.zip
 
 # 重新添加 luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
