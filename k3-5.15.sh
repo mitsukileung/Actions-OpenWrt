0ef4d9032c30925a439b13a949835f36ea5f6fb0
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2022-10-02 01:45:34 +0800

    Update k3-5.15.sh

--- k3-5.15.sh
+++ k3-5.15.sh
@@ -34,7 +34,7 @@ sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES 
 #1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
 #firmware='_asus-dhd24'
 echo '替换K3的无线驱动为asus-dhd24'
-#wget -nv https://github.com/xiangfeidexiaohuo/Phicomm-K3_Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+#wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 wget -nv https://github.com/xiangfeidexiaohuo/Phicomm-K3_Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin_asus-dhd24 -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 echo '=========Replace k3 wireless firmware OK!========='
 
