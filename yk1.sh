f9e5a7f1a5b0c606abf84860ba627e0d7c04667c
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2022-11-30 00:17:56 +0800

    Update yk1.sh

--- yk1.sh
+++ yk1.sh
@@ -6,18 +6,29 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: yk1.sh
+# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-# Modify Kernel 5.10
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/ramips/Makefile
+# Modify X86 Kernel 5.10
+# sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
-#zip -rP L824613 files.zip files
-#wget -O files.zip https://raw.githubusercontent.com/takayukileung/lede/master/target/linux/files.zip
-#unzip -P L824613 files.zip
-#rm files.zip
+# 重新添加 luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+
+# 添加 luci-app-unblockneteasemusic
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+
+# 添加 filebrowser
+git clone https://github.com/takayukileung/luci-app-filebrowser.git package/luci-app-filebrowser
+
+# 添加 OpenClash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openclash
+
+# 添加 luci-theme-neobird 主题
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
