99860af7e10ebf6f228c1efa4b561f43689cb23f
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-02-07 12:13:09 +0800

    Update k3.sh

--- k3.sh
+++ k3.sh
@@ -9,10 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# update frp to 0.46.1
-sed -i 's/0.45.0/0.46.1/g' feeds/packages/net/frp/Makefile
-sed -i 's/829cf9f14861ab1b074de6995282f30292f53513824372cfec4084a2e8de7123/af3e8d9d4144cf520cee2609cd45fb575afe711c03cc7441dc89d0402628a869/g' feeds/packages/net/frp/Makefile
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 # Modify X86 Kernel 5.10
