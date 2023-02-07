e65d46156019b159ca0e5afa0646eafa439c5ad9
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-02-08 01:39:19 +0800

    Update ax6-jp.sh

--- ax6-jp.sh
+++ ax6-jp.sh
@@ -25,9 +25,9 @@ sed -i 's/443/57002/g' feeds/luci/applications/luci-app-frpc/root/etc/config/frp
 sed -i 's/GO_VERSION_PATCH:=4/GO_VERSION_PATCH:=5/g' feeds/packages/lang/golang/golang/Makefile
 sed -i 's/eda74db4ac494800a3e66ee784e495bfbb9b8e535df924a8b01b1a8028b7f368/8e486e8e85a281fc5ce3f0bedc5b9d2dbf6276d7db0b25d3ec034f313da0375f/g' feeds/packages/lang/golang/golang/Makefile
 rm -rf feeds/packages/lang/golang/golang/patches/010-cmd-compile-turn-off-jump-tables-when-spectre-retpolines-.patch
-#update frp to 0.46.1
-sed -i 's/0.45.0/0.46.1/g' feeds/packages/net/frp/Makefile
-sed -i 's/829cf9f14861ab1b074de6995282f30292f53513824372cfec4084a2e8de7123/af3e8d9d4144cf520cee2609cd45fb575afe711c03cc7441dc89d0402628a869/g' feeds/packages/net/frp/Makefile
+# update frp to 0.46.1
+#sed -i 's/0.45.0/0.46.1/g' feeds/packages/net/frp/Makefile
+#sed -i 's/829cf9f14861ab1b074de6995282f30292f53513824372cfec4084a2e8de7123/af3e8d9d4144cf520cee2609cd45fb575afe711c03cc7441dc89d0402628a869/g' feeds/packages/net/frp/Makefile
 # vemss add 'alterId'
 #wget -O feeds/helloworld/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client-config.lua https://raw.githubusercontent.com/mitsukileung/mod/main/ssrp/client-config.lua
 #wget -O feeds/helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/ssrurl.htm https://raw.githubusercontent.com/mitsukileung/mod/main/ssrp/ssrurl.htm
