cf5c249bf2ead013903ec28791d597e6186b1111
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-02-08 01:39:56 +0800

    Update ax6.sh

--- ax6.sh
+++ ax6.sh
@@ -38,9 +38,9 @@ sed -i 's/8056c2e21c000001/1950791e780ce563/g' packages/net/zerotier/files/etc/c
 sed -i 's/GO_VERSION_PATCH:=4/GO_VERSION_PATCH:=5/g' feeds/packages/lang/golang/golang/Makefile
 sed -i 's/eda74db4ac494800a3e66ee784e495bfbb9b8e535df924a8b01b1a8028b7f368/8e486e8e85a281fc5ce3f0bedc5b9d2dbf6276d7db0b25d3ec034f313da0375f/g' feeds/packages/lang/golang/golang/Makefile
 rm -rf feeds/packages/lang/golang/golang/patches/010-cmd-compile-turn-off-jump-tables-when-spectre-retpolines-.patch
-#update frp to 0.46.1
-sed -i 's/0.45.0/0.46.1/g' feeds/packages/net/frp/Makefile
-sed -i 's/829cf9f14861ab1b074de6995282f30292f53513824372cfec4084a2e8de7123/af3e8d9d4144cf520cee2609cd45fb575afe711c03cc7441dc89d0402628a869/g' feeds/packages/net/frp/Makefile
+# update frp to 0.46.1
+#sed -i 's/0.45.0/0.46.1/g' feeds/packages/net/frp/Makefile
+#sed -i 's/829cf9f14861ab1b074de6995282f30292f53513824372cfec4084a2e8de7123/af3e8d9d4144cf520cee2609cd45fb575afe711c03cc7441dc89d0402628a869/g' feeds/packages/net/frp/Makefile
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
