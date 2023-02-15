1bc262ab3e0e435218f7bef1c23df390e267d591
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-02-15 17:10:36 +0800

    Create d2.sh

---
+++ d2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.28.1/g' package/base-files/files/bin/config_generate
+# Modify X86 Kernel 5.10
+# sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+# Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+
+# 重新添加 luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+
+# 添加 luci-app-unblockneteasemusic
+#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+
+# 添加 filebrowser
+git clone https://github.com/takayukileung/luci-app-filebrowser.git package/luci-app-filebrowser
+
+# 添加 OpenClash
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openclash
+
+# 添加 luci-theme-neobird 主题
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
