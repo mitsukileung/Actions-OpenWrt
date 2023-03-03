0e92d44beeac2b25d303692d463cf895370c8fe5
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-03-04 01:58:52 +0800

    Update k2p.sh

--- k2p.sh
+++ k2p.sh
@@ -11,11 +11,11 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.1.88/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 # Modify Kernel 5.10
-sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/ramips/Makefile
+#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/ramips/Makefile
 # Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 #zip -rP L824613 files.zip files
 #wget -O files.zip https://raw.githubusercontent.com/takayukileung/lede/master/target/linux/files.zip
