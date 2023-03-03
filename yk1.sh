579a74cd3bcd27e6db0620d32be10c0de958d9c8
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-03-04 05:57:53 +0800

    Update yk1.sh

--- yk1.sh
+++ yk1.sh
@@ -20,6 +20,11 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 wget -O files.zip https://raw.githubusercontent.com/takayukileung/lede/master/target/files.zip
 unzip -P Jp3895 files.zip
 rm files.zip
+# Modify frp
+sed -i 's/host upx\///g' feeds/packages/net/frp/Makefile
+sed -i '/lzma/d' feeds/packages/net/frp/Makefile
+sed -i 's/+wget/+wget-ssl/g' feeds/luci/applications/luci-app-frpc/Makefile
+sed -i 's/+wget/+wget-ssl/g' feeds/luci/applications/luci-app-frps/Makefile
 # update frp to 0.47.0
 #sed -i 's/0.46.1/0.47.0/g' feeds/packages/net/frp/Makefile
 #sed -i 's/af3e8d9d4144cf520cee2609cd45fb575afe711c03cc7441dc89d0402628a869/d98ebf8638701cf8662604b9fe2fb355f91209f87edcb77f2830c7fa40ccbe37/g' feeds/packages/net/frp/Makefile
