d6e673c00c22d4c9a1a10fda653b70065b629dbd
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-03-06 11:00:31 +0800

    Update yk1.sh

--- yk1.sh
+++ yk1.sh
@@ -17,9 +17,9 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 #zip -rP Jp3895 files.zip files
-#wget -O files.zip https://raw.githubusercontent.com/takayukileung/lede/master/target/files.zip
-#unzip -P Jp3895 files.zip
-#rm files.zip
+wget -O files.zip https://raw.githubusercontent.com/takayukileung/lede/master/target/files.zip
+unzip -P Jp3895 files.zip
+rm files.zip
 # Modify frp
 sed -i 's/host upx\///g' feeds/packages/net/frp/Makefile
 sed -i '/lzma/d' feeds/packages/net/frp/Makefile
