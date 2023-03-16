5d372ab9a38e7271dd1f2abe722451134e65aea4
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-03-16 17:36:43 +0800

    Update ax6.sh

--- ax6.sh
+++ ax6.sh
@@ -25,10 +25,13 @@ sed -i 's/443/57002/g' feeds/luci/applications/luci-app-frpc/root/etc/config/frp
 sed -i 's/enabled 0/enabled 1/g' packages/net/zerotier/files/etc/config/zerotier
 sed -i 's/8056c2e21c000001/1950791e780ce563/g' packages/net/zerotier/files/etc/config/zerotier
 # update to upx 4.0.2
-rm -rf package/lean/upx/*
-wget -O package/lean/upx/Makefile https://raw.githubusercontent.com/immortalwrt/packages/master/utils/upx/Makefile
-sed -i 's/4.0.1/4.0.2/g' package/lean/upx/Makefile
-sed -i 's/77003c8e2e29aa9804e2fbaeb30f055903420b3e01d95eafe01aed957fb7e190/1221e725b1a89e06739df27fae394d6bc88aedbe12f137c630ec772522cbc76f/g' package/lean/upx/Makefile
+#rm -rf package/lean/upx/*
+#wget -O package/lean/upx/Makefile https://raw.githubusercontent.com/immortalwrt/packages/master/utils/upx/Makefile
+#sed -i 's/4.0.1/4.0.2/g' package/lean/upx/Makefile
+#sed -i 's/77003c8e2e29aa9804e2fbaeb30f055903420b3e01d95eafe01aed957fb7e190/1221e725b1a89e06739df27fae394d6bc88aedbe12f137c630ec772522cbc76f/g' package/lean/upx/Makefile
+# update frp to 0.48.0
+sed -i 's/0.47.0/0.48.0/g' feeds/packages/net/frp/Makefile
+sed -i 's/d98ebf8638701cf8662604b9fe2fb355f91209f87edcb77f2830c7fa40ccbe37/efba8ec9fad3369ce62631369f52b78a7248df426b5b54311e96231adac5cc76/g' feeds/packages/net/frp/Makefile
 # Modify frp
 #sed -i 's/host upx\///g' feeds/packages/net/frp/Makefile
 #sed -i '/lzma/d' feeds/packages/net/frp/Makefile
