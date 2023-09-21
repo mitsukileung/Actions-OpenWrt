28428d1672eab0e37092c14e8b5563d04dfb4f9c
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-09-21 13:13:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,8 @@ sed -i 's/1234567/www.126126.xyz/g' feeds/luci/applications/luci-app-frpc/root/e
 sed -i "s/DISTRIB_DESCRIPTION='.*OpenWrt '/DISTRIB_DESCRIPTION='Mitsuki($(TZ=UTC-8 date +%Y.%m.%d))@OpenWrt '/g" package/lean/default-settings/files/zzz-default-settings
 
 # update zerotier config
-#sed -i 's/8056c2e21c000001/c026309d77879fdc/g' feeds/packages/net/zerotier/files/etc/config/zerotier
+sed -i 's/1.12.1/1.12.2/g' feeds/packages/net/zerotier/Makefile
+sed -i 's/c6758a04f161bba1c0ef11fce991029a645ede381ae3862a25a2f5145aaffca8/7c6512cfc208374ea9dc9931110e35f71800c34890e0f35991ea485aae66e31c/g' feeds/packages/net/zerotier/Makefile
 
 # update frp to 0.51.3
 sed -i 's/0.47.0/0.51.3/g' feeds/packages/net/frp/Makefile
