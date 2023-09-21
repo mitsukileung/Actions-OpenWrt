85acd7130e25b179b422ab66700872bb1cd7c6ec
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-09-21 17:18:32 +0800

    Update jp-diy-part2.sh

--- jp-diy-part2.sh
+++ jp-diy-part2.sh
@@ -22,6 +22,10 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/f
 #固件版本号添加个人标识和日期
 sed -i "s/DISTRIB_DESCRIPTION='.*OpenWrt '/DISTRIB_DESCRIPTION='Mitsuki($(TZ=UTC-8 date +%Y.%m.%d))@OpenWrt '/g" package/lean/default-settings/files/zzz-default-settings
 
+# update zerotier config
+sed -i 's/1.12.1/1.12.2/g' feeds/packages/net/zerotier/Makefile
+sed -i 's/c6758a04f161bba1c0ef11fce991029a645ede381ae3862a25a2f5145aaffca8/7c6512cfc208374ea9dc9931110e35f71800c34890e0f35991ea485aae66e31c/g' feeds/packages/net/zerotier/Makefile
+
 # update frp to 0.51.3
 sed -i 's/0.47.0/0.51.3/g' feeds/packages/net/frp/Makefile
 sed -i 's/d98ebf8638701cf8662604b9fe2fb355f91209f87edcb77f2830c7fa40ccbe37/83032399773901348c660d41c967530e794ab58172ccd070db89d5e50d915fef/g' feeds/packages/net/frp/Makefile
