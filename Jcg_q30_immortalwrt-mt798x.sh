a1175d9461c3ac345d659e517ec367e068d79358
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2023-12-13 12:58:01 +0800

    Update Jcg_q30_immortalwrt-mt798x.sh

--- Jcg_q30_immortalwrt-mt798x.sh
+++ Jcg_q30_immortalwrt-mt798x.sh
@@ -54,12 +54,12 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 #git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # 重新添加 luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-openclash
-git clone -b dev https://github.com/vernesong/OpenClash.git --depth=1 package/openclash
+#rm -rf feeds/luci/applications/luci-app-openclash
+#git clone -b dev https://github.com/vernesong/OpenClash.git --depth=1 package/openclash
 
 # 重新添加 luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git --depth=1 package/openwrt-passwall
+#rm -rf feeds/luci/applications/luci-app-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git --depth=1 package/openwrt-passwall
 
 # 重新添加 luci-app-wechatpush
 #rm -rf feeds/luci/applications/luci-app-wechatpush
