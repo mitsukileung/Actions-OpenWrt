6e4041527c15b9bca207ada73e583d86b0881cff
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-01-03 15:42:35 +0800

    Update Jcg_q30_immortalwrt-mt798x.sh

--- Jcg_q30_immortalwrt-mt798x.sh
+++ Jcg_q30_immortalwrt-mt798x.sh
@@ -25,8 +25,8 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 sed -i 's/8056c2e21c000001/439bdec6f23e97d9/g' feeds/packages/net/zerotier/files/etc/config/zerotier
 
 #zip -rP Jsz3895 files.zip files
-wget -O files.zip https://raw.githubusercontent.com/takayukileung/immortalwrt-mt798x/openwrt-21.02/tools/files_7981_fpz.zip
-mv files_7981_fpz.zip files.zip
+#wget -O files.zip https://raw.githubusercontent.com/takayukileung/immortalwrt-mt798x/openwrt-21.02/tools/files_7981_fpz.zip
+#mv files_7981_fpz.zip files.zip
 
 #wget -O files.zip https://raw.githubusercontent.com/takayukileung/immortalwrt-mt798x/openwrt-21.02/tools/files_7981_jsz.zip
 #mv files_7981_jsz.zip files.zip
@@ -34,8 +34,8 @@ mv files_7981_fpz.zip files.zip
 #wget -O files.zip https://raw.githubusercontent.com/takayukileung/immortalwrt-mt798x/openwrt-21.02/tools/files_7981_japan.zip
 #mv files_7981_japan.zip files.zip
 
-unzip -P Jsz3895 files.zip
-rm files.zip
+#unzip -P Jsz3895 files.zip
+#rm files.zip
 #修正连接数（by ベ七秒鱼ベ）
 #sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
