b88d473abba623a69b4a754b0fba86fa008439a8
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-01-25 03:25:01 +0800

    Update ax3600_immortalwrt_part2.sh

--- ax3600_immortalwrt_part2.sh
+++ ax3600_immortalwrt_part2.sh
@@ -24,7 +24,7 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 
 sed -i 's/8056c2e21c000001/439bdec6f23e97d9/g' feeds/packages/net/zerotier/files/etc/config/zerotier
 
-sed -i 's/949feec1da2bc9d43b6c766c1dfb6f71f737a221e5ce4220616a3900dfb40c82/949feec1da2bc9d43b6c766c1dfb6f71.8.0a221e5ce4220616a3900dfb40c82/g' feeds/packages/net/sing-box/Makefile
+#sed -i 's/949feec1da2bc9d43b6c766c1dfb6f71f737a221e5ce4220616a3900dfb40c82/949feec1da2bc9d43b6c766c1dfb6f71.8.0a221e5ce4220616a3900dfb40c82/g' feeds/packages/net/sing-box/Makefile
 
 #sed -i 's/1.7.7/1.8.0/g' feeds/packages/net/sing-box/Makefile
 #sed -i 's/ce182cb2181e898b56ca9b6ce0d5adeaece8e761ac62ce8cde69b3c7219b8430/80ae2a860fc77d961c578999e5fcfe964f969c81d9ccac156b2fef1340eca12f/g' feeds/packages/net/sing-box/Makefile
