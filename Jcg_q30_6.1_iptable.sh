246b59a38c6bd1f90da792db40ca816b8758bbad
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-01-29 21:31:51 +0800

    Update Jcg_q30_6.1_iptable.sh

--- Jcg_q30_6.1_iptable.sh
+++ Jcg_q30_6.1_iptable.sh
@@ -70,16 +70,16 @@ sed -i 's/8056c2e21c000001/439bdec6f23e97d9/g' feeds/packages/net/zerotier/files
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 重新添加 luci-app-homeproxy dev版
-rm -rf /feeds/luci/applications/luci-app-homeproxy
-git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
+#rm -rf /feeds/luci/applications/luci-app-homeproxy
+#git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # 重新添加 luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone -b dev https://github.com/vernesong/OpenClash.git --depth=1 package/luci-app-openclash
 
 # 重新添加 luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git --depth=1 package/luci-app-passwall
+#rm -rf feeds/luci/applications/luci-app-passwall
+#git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git --depth=1 package/luci-app-passwall
 
 # 重新添加 luci-app-socat 同时兼容firewall3/4 
 #rm -rf feeds/luci/applications/luci-app-socat
