9dd216f469db99a483b4ef7fe3501e697a6a17a7
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-03-05 11:11:57 +0800

    Update ax3600_lean_part1.sh

--- ax3600_lean_part1.sh
+++ ax3600_lean_part1.sh
@@ -23,4 +23,5 @@
 # Add a feed source
 #echo 'src-git-full helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
