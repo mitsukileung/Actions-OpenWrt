f0a93cd638db5d8b967d2890d2582e5b0c7a6d5b
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-04-14 23:53:30 +0800

    Update Jcg_q30_immortalwrt-mt798x_part1.sh

--- Jcg_q30_immortalwrt-mt798x_part1.sh
+++ Jcg_q30_immortalwrt-mt798x_part1.sh
@@ -23,3 +23,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
 #echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+./scripts/feeds update -a
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a
