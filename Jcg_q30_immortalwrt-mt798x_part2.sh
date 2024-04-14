2a6d3944e5aa805d1c3d335de8f61589f270a033
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-04-14 23:54:30 +0800

    Update Jcg_q30_immortalwrt-mt798x_part2.sh

--- Jcg_q30_immortalwrt-mt798x_part2.sh
+++ Jcg_q30_immortalwrt-mt798x_part2.sh
@@ -28,6 +28,8 @@ rm -f feeds/packages/net/socat/patches/100-usleep.patch
 sed -i 's/1.7.3.4/1.8.0.0/g' feeds/packages/net/socat/Makefile
 sed -i 's/972374ca86f65498e23e3259c2ee1b8f9dbeb04d12c2a78c0c9b5d1cb97dfdfc/e1de683dd22ee0e3a6c6bbff269abe18ab0c9d7eb650204f125155b9005faca7/g' feeds/packages/net/socat/Makefile
 
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+
 #rm -rf feeds/packages/net/tailscale/patches
 #sed -i 's/1.32.3/1.38.3/g' feeds/packages/net/tailscale/Makefile
 #sed -i 's/4cf88a1d754240ce71b29d3a65ca480091ad9c614ac99c541cef6fdaf0585dd4/63ef41e642f4b2c322c794742f03e9e06b593992e9a1299c94f495a496cffb3a/g' feeds/packages/net/tailscale/Makefile
