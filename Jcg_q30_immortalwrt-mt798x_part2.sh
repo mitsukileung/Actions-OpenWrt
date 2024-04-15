ce2fc26511d25e2455f4c641b2795c86bf860ef8
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-04-16 02:23:56 +0800

    Update Jcg_q30_immortalwrt-mt798x_part2.sh

--- Jcg_q30_immortalwrt-mt798x_part2.sh
+++ Jcg_q30_immortalwrt-mt798x_part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.98.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.3/g' package/base-files/files/bin/config_generate
 # Modify X86 Kernel 5.10
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/ipq807x/Makefile
 # Clear the login password
