82b37064e2b76bee209b7accaae9984b8a41873d
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-04-19 13:21:56 +0800

    Update X64_immortalwrt_23.05_part2.sh

--- X64_immortalwrt_23.05_part2.sh
+++ X64_immortalwrt_23.05_part2.sh
@@ -7,7 +7,8 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Description: OpenWrt DIY script part 2 (After Update f
+eeds)
 #
 
 # Modify default IP
@@ -64,7 +65,7 @@ sed -i 's/fbd42bd2f0e54a3af6d01bdf15385384ab82dbc0e4f1a5e153b3e0be1b6380ac/e1de6
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 重新添加 luci-app-homeproxy dev版
-rm -rf /feeds/luci/applications/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # 重新添加 luci-app-openclash
