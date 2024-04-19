318729d3ea377a7f7e90b988a2f75cf145ed4d01
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-04-20 02:10:13 +0800

    Update X64_immortalwrt_23.05_part2.sh

--- X64_immortalwrt_23.05_part2.sh
+++ X64_immortalwrt_23.05_part2.sh
@@ -7,8 +7,7 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update f
-eeds)
+# Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
 # Modify default IP
