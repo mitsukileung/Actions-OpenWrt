62e0287e1017b819877eb9c6e9438b014165fcf0
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-04-21 23:08:48 +0800

    Update Jcg_test_part1.sh

--- Jcg_test_part1.sh
+++ Jcg_test_part1.sh
@@ -27,7 +27,7 @@ sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a
 rm -rf feeds/packages/lang/golang
 #rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,pdnsd-alt,redsocks2,shadow-tls,shadowsocks-rust,simple-obfs,mosdns,tcping,trojan*,tuic-client,xray*,v2ray*}
-#rm -rf feeds/small/shadowsocksr-libev
+rm -rf feeds/small/shadowsocksr-libev
 #sed -i '/iptables-mod-socket/d' feeds/small/luci-app-passwall/Makefile
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a
