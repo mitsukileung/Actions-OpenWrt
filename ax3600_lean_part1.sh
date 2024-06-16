7087b190a4d7c5c6702883a42d2d7ab973a5303c
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-06-17 04:08:43 +0800

    Update ax3600_lean_part1.sh

--- ax3600_lean_part1.sh
+++ ax3600_lean_part1.sh
@@ -24,8 +24,8 @@
 #echo 'src-git-full helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
 
