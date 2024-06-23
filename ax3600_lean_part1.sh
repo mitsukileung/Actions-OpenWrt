61e063b62e30fd2065a33fd0f690317d9dda3e15
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-06-23 18:13:20 +0800

    Update ax3600_lean_part1.sh

--- ax3600_lean_part1.sh
+++ ax3600_lean_part1.sh
@@ -20,6 +20,7 @@ sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
 #rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*}
 #wget -O feeds/small/sing-box/Makefile https://raw.githubusercontent.com/takayukileung/JB/main/Makefile-sing-box
 
