274bf93bc54e5d18060dd5118c0f6b9cfbfe317a
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-07-26 12:56:50 +0800

    Update ax3600_immortalwrt_part2.sh

--- ax3600_immortalwrt_part2.sh
+++ ax3600_immortalwrt_part2.sh
@@ -31,8 +31,8 @@ sed -i 's/8056c2e21c000001/9f77fc393e758059/g' feeds/packages/net/zerotier/files
 
 #wget -O feeds/packages/net/chinadns-ng/Makefile https://raw.githubusercontent.com/kenzok8/small/master/chinadns-ng/Makefile
 
-sed -i 's/1.68.0/1.68.1/g' feeds/packages/net/tailscale/Makefile
-sed -i 's/b217e4190e38b9b9799c7749307d207385979ee6da95a16634fc7279d1658314/d7fe30282d2f5eabdc76a5a89f11d935ed3a5d93d55f5fd5b40f9a9f49e19490/g' feeds/packages/net/tailscale/Makefile
+sed -i 's/1.68.2/1.70.0/g' feeds/packages/net/tailscale/Makefile
+sed -i 's/9d34bd153c485dd0d88d3d76f187b5032046c0807a411ca97f38c8039a9ac659/8429728708f9694534489daa0a30af58be67f25742597940e7613793275c738f/g' feeds/packages/net/tailscale/Makefile
 
 #rm -f feeds/packages/net/socat/patches/100-usleep.patch
 #sed -i 's/1.7.4.4/1.8.0.0/g' feeds/packages/net/socat/Makefile
