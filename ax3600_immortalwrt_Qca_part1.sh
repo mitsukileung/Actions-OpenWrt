653ca6e91302038d0e2e2f2e3a58b6102ecc42c3
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-08-24 14:10:42 +0800

    Update ax3600_immortalwrt_Qca_part1.sh

--- ax3600_immortalwrt_Qca_part1.sh
+++ ax3600_immortalwrt_Qca_part1.sh
@@ -19,19 +19,19 @@
 #wget -O target/linux/ipq807x/image/generic.mk https://raw.githubusercontent.com/mitsukileung/Actions-OpenWrt/main/mi_patch/generic.mk
 
 #sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
-#./scripts/feeds update -a
-#git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
-#rm -rf feeds/packages/net/mosdns
-#wget -O test1.sh https://raw.githubusercontent.com/takayukileung/JB/main/v2ray-geodata_update.sh
+./scripts/feeds update -a
+git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+rm -rf feeds/packages/net/mosdns
+wget -O test1.sh https://raw.githubusercontent.com/takayukileung/JB/main/v2ray-geodata_update.sh
 #wget -O feeds/packages/net/chinadns-ng/Makefile https://raw.githubusercontent.com/kenzok8/small/master/chinadns-ng/Makefile
-#chmod 777 test1.sh
-#./test1.sh
+chmod 777 test1.sh
+./test1.sh
 #rm -rf feeds/packages/lang/golang
 #rm -rf feeds/luci/applications/luci-app-passwall
-#rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
 #git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-#rm -f ./test1.sh
-#./scripts/feeds install -a
+rm -f ./test1.sh
+./scripts/feeds install -a
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
