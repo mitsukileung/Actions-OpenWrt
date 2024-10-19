7fddebd9aa08af81f599ae17670230a759d97b61
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-10-20 03:01:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '/luci/d' feeds.conf.default
+sed -i '2i src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 #sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a
 rm -rf feeds/luci/applications/luci-app-serverchan
