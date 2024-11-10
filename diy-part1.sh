54fdd0665c123aaa6f7f547fb230e3a73b3bbf60
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-11-10 19:23:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,17 +14,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '/luci/d' feeds.conf.default
-sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+sed -i '2i src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+#sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a
-rm -rf feeds/luci/applications/luci-app-frpc
+rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{mosdns,xray*,v2ray*,v2ray*,sing*}
 rm -rf feeds/packages/utils/v2dat
-#rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*}
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,microsocks,mosdns,pdnsd-alt,sing-box,tcping,v2ray-geodata,xray-core}
 #wget -O feeds/small/sing-box/Makefile https://raw.githubusercontent.com/takayukileung/JB/main/Makefile-sing-box
 
 #wget -O feeds/small/sing-box/Makefile https://raw.githubusercontent.com/immortalwrt/packages/master/net/sing-box/Makefile
@@ -32,7 +29,6 @@ rm -rf feeds/packages/utils/v2dat
 #wget -O feeds/small/sing-box/files/sing-box.init https://raw.githubusercontent.com/immortalwrt/packages/master/net/sing-box/files/sing-box.init
 #wget -O feeds/small/sing-box/files/sing-box.conf https://raw.githubusercontent.com/immortalwrt/packages/master/net/sing-box/files/sing-box.conf
 #rm -f feeds/small/sing-box/files/config.json.example
-
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a 
