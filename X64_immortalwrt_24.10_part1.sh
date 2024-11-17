8014124cd3dabecc9bfb095d7e8656f8ef76beed
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-11-17 21:06:33 +0800

    Update and rename X64_immortalwrt_23.05_part1.sh to X64_immortalwrt_24.10_part1.sh

--- X64_immortalwrt_24.10_part1.sh
+++ X64_immortalwrt_24.10_part1.sh
@@ -18,12 +18,25 @@
 # 添加xiaomi系列
 #wget -O target/linux/ipq807x/image/generic.mk https://raw.githubusercontent.com/mitsukileung/Actions-OpenWrt/main/mi_patch/generic.mk
 
-sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a
-wget -O feeds/packages/net/tailscale/Makefile https://raw.githubusercontent.com/immortalwrt/packages/master/net/tailscale/Makefile
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
+git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+#wget -O test1.sh https://raw.githubusercontent.com/takayukileung/JB/main/v2ray-geodata_update.sh
+#wget -O feeds/packages/net/chinadns-ng/Makefile https://raw.githubusercontent.com/kenzok8/small/master/chinadns-ng/Makefile
+#chmod 777 test1.sh
+#./test1.sh
+
+#sed -i 's/kmod-button-hotplug/kmod-button-hotplug kmod-drm-i915/g' target/linux/x86/Makefile
+
+#rm -rf feeds/packages/lang/golang
+#rm -rf feeds/luci/applications/luci-app-passwall
+#rm -rf feeds/luci/applications/luci-app-openclash
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -f ./test1.sh
+./scripts/feeds install -a
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
