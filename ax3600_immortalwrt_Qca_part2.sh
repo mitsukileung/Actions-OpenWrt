25be3b1133c67dd7ae76ab1d9efe536edc56807c
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-11-18 14:27:48 +0800

    Update ax3600_immortalwrt_Qca_part2.sh

--- ax3600_immortalwrt_Qca_part2.sh
+++ ax3600_immortalwrt_Qca_part2.sh
@@ -85,12 +85,12 @@ rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # 重新添加 luci-app-openclash
-#rm -rf feeds/luci/applications/luci-app-openclash
-#git clone --filter=blob:none --branch=dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone --filter=blob:none --branch=dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # 重新添加 luci-app-passwall
-#rm -rf feeds/luci/applications/luci-app-passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 
 # 重新添加 luci-app-socat 同时兼容firewall3/4 
 #rm -rf feeds/luci/applications/luci-app-socat
