52432c1083075113e3a52c9b822b99e826717934
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2024-12-03 12:07:19 +0800

    Update Jcg_q30_padavanonly_23.05_part2.sh

--- Jcg_q30_padavanonly_23.05_part2.sh
+++ Jcg_q30_padavanonly_23.05_part2.sh
@@ -24,8 +24,8 @@ sed -i 's/192.168.6.1/192.168.88.3/g' package/base-files/files/bin/config_genera
 
 sed -i 's/8056c2e21c000001/9f77fc393e758059/g' feeds/packages/net/zerotier/files/etc/config/zerotier
 
-mkdir package/v2ray-geoview
-wget -O package/v2ray-geoview/Makefile https://raw.githubusercontent.com/xiaorouji/openwrt-passwall-packages/refs/heads/main/v2ray-geoview/Makefile
+#mkdir package/v2ray-geoview
+#wget -O package/v2ray-geoview/Makefile https://raw.githubusercontent.com/xiaorouji/openwrt-passwall-packages/refs/heads/main/v2ray-geoview/Makefile
 
 #rm -rf feeds/packages/net/mosdns
 #rm -rf feeds/packages/net/v2ray-geodata
