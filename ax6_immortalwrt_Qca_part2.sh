3cf36dd92f51dfffdad6a5dd97c4f9996cdacd0c
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2025-01-20 02:25:30 +0800

    Update ax6_immortalwrt_Qca_part2.sh

--- ax6_immortalwrt_Qca_part2.sh
+++ ax6_immortalwrt_Qca_part2.sh
@@ -85,8 +85,8 @@ git clone https://github.com/asvow/luci-app-tailscale.git package/luci-app-tails
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 重新添加 luci-app-homeproxy dev版
-rm -rf feeds/luci/applications/luci-app-homeproxy
-git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
+#rm -rf feeds/luci/applications/luci-app-homeproxy
+#git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # 重新添加 luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
