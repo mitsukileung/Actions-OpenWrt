d7ae1a5b5e68c35ae32300f8ed67bfb2fed5687c
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2025-02-09 23:19:43 +0800

    Update ax6_immortalwrt_Qca_part2.sh

--- ax6_immortalwrt_Qca_part2.sh
+++ ax6_immortalwrt_Qca_part2.sh
@@ -22,8 +22,10 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/80/57001/g' feeds/luci/applications/luci-app-frpc/root/etc/config/frp
 #sed -i 's/443/57002/g' feeds/luci/applications/luci-app-frpc/root/etc/config/frp
 
+rm -rf feeds/luci/applications/luci-app-zerotier
+git clone https://github.com/takayukileung/luci-app-zerotier.git package/luci-app-zerotier
 sed -i 's/8056c2e21c000001/9f77fc393e758059/g' feeds/packages/net/zerotier/files/etc/config/zerotier
-sed -i 's/8056c2e21c000001/9f77fc393e758059/g' feeds/luci/applications/luci-app-zerotier/root/etc/config/zerotier
+#sed -i 's/8056c2e21c000001/9f77fc393e758059/g' feeds/luci/applications/luci-app-zerotier/root/etc/config/zerotier
 
 rm -rf feeds/packages/net/geoview
 mkdir package/v2ray-geoview
