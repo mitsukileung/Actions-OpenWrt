5c3148f042928c1efc177384864b925dc159d743
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2025-02-11 18:34:56 +0800

    Update ax3600_immortalwrt_Qca_part2.sh

--- ax3600_immortalwrt_Qca_part2.sh
+++ ax3600_immortalwrt_Qca_part2.sh
@@ -106,8 +106,8 @@ git clone https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-so
 #rm -rf feeds/luci/applications/luci-app-wechatpush
 #git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
 
-# 添加 OpenAppFilter
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# 添加 OpenWrt-nikki
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
 
 # 添加 smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
