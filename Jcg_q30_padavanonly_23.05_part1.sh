487f5d0dbdbfcd84a87febe4a7544ec8f59d55db
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2025-03-09 01:45:46 +0800

    Update Jcg_q30_padavanonly_23.05_part1.sh

--- Jcg_q30_padavanonly_23.05_part1.sh
+++ Jcg_q30_padavanonly_23.05_part1.sh
@@ -36,7 +36,7 @@ rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 #rm -f ./test1.sh
 ./scripts/feeds install -a
