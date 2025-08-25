d37ac716ffa9f66999de2a6aef2bed413df20e84
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2025-08-25 19:03:04 +0800

    Update sl-3000_padavanonly_24.10_part2.sh

--- sl-3000_padavanonly_24.10_part2.sh
+++ sl-3000_padavanonly_24.10_part2.sh
@@ -110,8 +110,8 @@ wget -O defconfig/mt7981-ax3000.config https://raw.githubusercontent.com/takayuk
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 重新添加 luci-app-homeproxy dev版
-rm -rf feeds/luci/applications/luci-app-homeproxy
-git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
+#rm -rf feeds/luci/applications/luci-app-homeproxy
+#git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # 重新添加 luci-app-openclash
 #rm -rf feeds/luci/applications/luci-app-openclash
