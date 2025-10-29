67815ba813b1e90d15c99fe0ecbd814e28dc0780
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2025-10-29 15:38:42 +0800

    Update Jcg_q30_NekokeCore_24.10_6.6_part2.sh

--- Jcg_q30_NekokeCore_24.10_6.6_part2.sh
+++ Jcg_q30_NekokeCore_24.10_6.6_part2.sh
@@ -103,8 +103,6 @@ rm -rf feeds/luci/applications/luci-app-openclash
 git clone --filter=blob:none --branch=dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # 重新添加 luci-app-passwall
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 
