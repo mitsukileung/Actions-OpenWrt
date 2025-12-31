70079207406b02c5745c40a00e6e05b5357ff5bf
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2025-12-31 19:05:42 +0800

    Update ax6_immortalwrt_Qca_part2_X.sh

--- ax6_immortalwrt_Qca_part2_X.sh
+++ ax6_immortalwrt_Qca_part2_X.sh
@@ -43,7 +43,7 @@ sed -i 's/9dc1d277be21851fbb7e4e7376f8b9cfe2d47b6ade9cf4459de35596e20782b6/20ee5
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+sed -i 's/ca-certificates/ca-bundle/g' feeds/packages/net/ddns-scripts_dnspod/Makefile
 
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
