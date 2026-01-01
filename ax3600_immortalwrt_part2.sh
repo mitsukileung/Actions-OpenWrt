901f46cb9a8bd8d1000a7f016f1a7d1c34a2075c
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-01-01 09:54:58 +0800

    Update ax3600_immortalwrt_part2.sh

--- ax3600_immortalwrt_part2.sh
+++ ax3600_immortalwrt_part2.sh
@@ -25,8 +25,6 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 
 sed -i 's/8056c2e21c000001/9f77fc393e758059/g' feeds/packages/net/zerotier/files/etc/config/zerotier
 
-wget -O package/system/ca-certificates/Makefile https://raw.githubusercontent.com/mitsukileung/Actions-OpenWrt/refs/heads/main/mi_patch/ca-certificates-makefile
-
 rm -rf feeds/packages/net/geoview
 mkdir package/geoview
 wget -O package/geoview/Makefile https://raw.githubusercontent.com/xiaorouji/openwrt-passwall-packages/refs/heads/main/geoview/Makefile
