b51c0c38ba1b280c3cb3f534ee54b2313b3155e6
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-01-11 20:03:02 +0800

    Update Jcg_q30_NekokeCore_24.10_6.6_part2.sh

--- Jcg_q30_NekokeCore_24.10_6.6_part2.sh
+++ Jcg_q30_NekokeCore_24.10_6.6_part2.sh
@@ -110,7 +110,7 @@ git clone --filter=blob:none --branch=dev https://github.com/vernesong/OpenClash
 
 # 重新添加 luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
 
 # 重新添加 luci-app-socat 同时兼容firewall3/4 
 #rm -rf feeds/luci/applications/luci-app-socat
