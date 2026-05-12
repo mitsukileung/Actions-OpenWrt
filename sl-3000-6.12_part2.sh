fdc9a7033039bfaa008907e98649074fcd108a0e
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-05-12 20:44:37 +0800

    Update sl-3000-6.12_part2.sh

--- sl-3000-6.12_part2.sh
+++ sl-3000-6.12_part2.sh
@@ -86,7 +86,8 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # 重新添加 luci-app-homeproxy dev版
 rm -rf feeds/luci/applications/luci-app-homeproxy
-git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
+#git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
+git clone https://github.com/VIKINGYFY/homeproxy.git package/luci-app-homeproxy
 
 # 重新添加 luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
