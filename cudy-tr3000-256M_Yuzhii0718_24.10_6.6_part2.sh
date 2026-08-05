5f3860c5f78c71c079510540006ffddfbacf37a4
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-08-05 09:16:50 +0800

    Update cudy-tr3000-256M_Yuzhii0718_24.10_6.6_part2.sh

--- cudy-tr3000-256M_Yuzhii0718_24.10_6.6_part2.sh
+++ cudy-tr3000-256M_Yuzhii0718_24.10_6.6_part2.sh
@@ -106,13 +106,14 @@ rm files.zip
 git clone https://github.com/derisamedia/luci-theme-alpha-reborn.git package/luci-theme-alpha-reborn
 
 # 重新添加 luci-app-homeproxy
-#rm -rf feeds/luci/applications/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
+git clone https://github.com/XiaoHaiSly/luci-app-homeproxy.git package/luci-app-homeproxy
 #git clone https://github.com/szwjp/homeproxy.git package/luci-app-homeproxy
 #mkdir package/sing-box
 #git clone https://github.com/VIKINGYFY/homeproxy.git package/luci-app-homeproxy
 #git clone https://github.com/VIKINGYFY/packages.git package/other
 #cp -r package/other/luci-app-homeproxy package/luci-app-homeproxy
-#rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/sing-box
 #cp -r package/other/sing-box package/sing-box
 #wget -O package/sing-box/Makefile https://raw.githubusercontent.com/Openwrt-Passwall/openwrt-passwall-packages/refs/heads/main/sing-box/Makefile
 #rm -rf package/other
