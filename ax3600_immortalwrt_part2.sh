0d92be4a506392b9537a0a8a0ac386eb17c59a67
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-08-14 15:07:29 +0800

    Update ax3600_immortalwrt_part2.sh

--- ax3600_immortalwrt_part2.sh
+++ ax3600_immortalwrt_part2.sh
@@ -152,8 +152,12 @@ rm -rf feeds/luci/applications/luci-app-ipsec-vpnd
 rm -rf feeds/luci/applications/luci-app-openvpn-server
 git clone https://github.com/szwjp/luci-app-ipsec-vpnd.git package/luci-app-ipsec-vpnd
 git clone https://github.com/szwjp/luci-app-openvpn-server.git package/luci-app-openvpn-server
-sed -i 's/2026070800/2026080300/g' feeds/packages/kernel/ovpn-dco/Makefile
-sed -i 's/2be764534b9a1c2e7827bc7fb832cf89827c5b2e73db6805cb749458e0c72e3b/b612261015b75fcb52a364d7bedc1e5fd70656e26ba388f1d88f58fc5fbb9560/g' feeds/packages/kernel/ovpn-dco/Makefile‎
+# 修改 feeds/packages/kernel/ovpn-dco
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=7.1.0.2026080300/g' feeds/packages/kernel/ovpn-dco/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=b612261015b75fcb52a364d7bedc1e5fd70656e26ba388f1d88f58fc5fbb9560/g' feeds/packages/kernel/ovpn-dco/Makefile
+# 修改 feeds/packages/net/openvpn/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.7.6/g' feeds/packages/net/openvpn/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=10e24a9385f23cc38cc5cf448f3ca0769f939bc4cbecc4f4647d7e006e52db74/g' feeds/packages/net/openvpn/Makefile‎
 
 # 添加 smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
