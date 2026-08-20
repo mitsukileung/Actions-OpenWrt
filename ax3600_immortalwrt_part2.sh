07f825db49f2db96519834fcb0ebf9a08773251d
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-08-20 15:26:28 +0800

    Update ax3600_immortalwrt_part2.sh

--- ax3600_immortalwrt_part2.sh
+++ ax3600_immortalwrt_part2.sh
@@ -19,6 +19,9 @@ sed -i "s/(luciversion || '')/& + (' \/ Mitsuki-$(TZ=UTC-8 date +%y.%m.%d)')/g" 
 # 修改K3固件大小
 wget -O target/linux/bcm53xx/image/Makefile https://raw.githubusercontent.com/mitsukileung/Actions-OpenWrt/refs/heads/main/K3_patch/k3_image_Makefile
 
+# 关闭 CI llvm
+sed -i 's/llvm=true/llvm=false/g' feeds/packages/lang/rust/Makefile
+
 # Modify X86 Kernel 5.10
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/ipq807x/Makefile
 # Clear the login password
