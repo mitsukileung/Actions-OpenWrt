6dcd2425bca5ff44420e8ca87828819315341471
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-09-01 10:12:11 +0800

    Update Jcg_q30_yuzhii0718_24.10_6.6_part2.sh

--- Jcg_q30_yuzhii0718_24.10_6.6_part2.sh
+++ Jcg_q30_yuzhii0718_24.10_6.6_part2.sh
@@ -100,6 +100,7 @@ git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git pa
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone https://github.com/VIKINGYFY/packages.git package/vikingyfy
+sed -i 's/1.14.0_alpha1/1.14.0/g' package/vikingyfy/luci-app-homeproxy/Makefile
 
 # 重新添加 luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
