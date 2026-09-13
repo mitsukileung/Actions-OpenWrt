fcc486a2a4a2b8c919dde1281b0df67b2fe07a31
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-09-13 23:37:51 +0800

    Update ax3600_immortalwrt_part2.sh

--- ax3600_immortalwrt_part2.sh
+++ ax3600_immortalwrt_part2.sh
@@ -98,6 +98,10 @@ sed -i "s/nav_type '.*'/nav_type 'dropdown'/g" $(find ./package/luci-app-aurora-
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone https://github.com/VIKINGYFY/packages.git package/vikingyfy
+
+sed -i 's/1.15.0-alpha.2/1.15.0-alpha.3/g' package/vikingyfy/sing-box/Makefile
+sed -i 's/1.15.0_alpha2/1.15.0_alpha3/g' package/vikingyfy/sing-box/Makefile
+sed -i 's/76f322be1e86b689dfdb12be9d27314e7a329dca03b134222c7454900321f8be/152a2700feed70b28673b93debbf59806480eb540a933fcd59b44eb9c6bc4cf9/g' package/vikingyfy/sing-box/Makefile
 sed -i 's/1.14.0_alpha1/1.14.0/g' package/vikingyfy/luci-app-homeproxy/Makefile
 
 # 重新添加 luci-app-openclash
@@ -116,7 +120,7 @@ wnflb99.com\
 555gy.cc\
 555zxdy.cc\
 666ys.net\
-91zxdy.com/\
+91zxdy.com\
 ' package/passwall-luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 
 git clone https://github.com/fcshark-org/openwrt-fchomo.git package/openwrt-fchomo
