d98d2a7042902d03f17c95e5f06c4d851d1a3351
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-09-15 06:34:01 +0800

    Update Jcg_q30_yuzhii0718_24.10_6.6_part2.sh

--- Jcg_q30_yuzhii0718_24.10_6.6_part2.sh
+++ Jcg_q30_yuzhii0718_24.10_6.6_part2.sh
@@ -104,7 +104,7 @@ git clone https://github.com/VIKINGYFY/packages.git package/vikingyfy
 #sed -i 's/1.15.0-alpha.2/1.15.0-alpha.3/g' package/vikingyfy/sing-box/Makefile
 #sed -i 's/1.15.0_alpha2/1.15.0_alpha3/g' package/vikingyfy/sing-box/Makefile
 #sed -i 's/76f322be1e86b689dfdb12be9d27314e7a329dca03b134222c7454900321f8be/152a2700feed70b28673b93debbf59806480eb540a933fcd59b44eb9c6bc4cf9/g' package/vikingyfy/sing-box/Makefile
-#sed -i 's/1.14.0_alpha1/1.14.0/g' package/vikingyfy/luci-app-homeproxy/Makefile
+sed -i 's/1.15.0/1.15.0_alpha3/g' package/vikingyfy/luci-app-homeproxy/Makefile
 
 # 重新添加 luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
