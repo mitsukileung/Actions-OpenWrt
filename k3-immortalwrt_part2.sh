c91a3d9afb066cbe7d52c5f2f7b7a3500deec341
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2026-09-18 18:56:57 +0800

    Update k3-immortalwrt_part2.sh

--- k3-immortalwrt_part2.sh
+++ k3-immortalwrt_part2.sh
@@ -100,9 +100,9 @@ rm -rf feeds/packages/net/sing-box
 rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone https://github.com/VIKINGYFY/packages.git package/vikingyfy
 
-sed -i 's/1.15.0-alpha.4/1.15.0-alpha.5/g' package/vikingyfy/sing-box/Makefile
-sed -i 's/1.15.0_alpha4/1.15.0_alpha5/g' package/vikingyfy/sing-box/Makefile
-sed -i 's/d597a18f8624968a53ab9286f3a515270d3e1dc18600cc7e1d45809997cbe1fd/966eaaf0d1f0ee159f8b6f64e3458b4ee08d941c0f39a70f82675d548b857d79/g' package/vikingyfy/sing-box/Makefile
+sed -i 's/1.15.0-alpha.5/1.15.0-alpha.6/g' package/vikingyfy/sing-box/Makefile
+sed -i 's/1.15.0_alpha5/1.15.0_alpha6/g' package/vikingyfy/sing-box/Makefile
+sed -i 's/966eaaf0d1f0ee159f8b6f64e3458b4ee08d941c0f39a70f82675d548b857d79/0eae23477162ea918b6481a9287658e6948aa22e26deb614fcb1fc7925cf9a5e/g' package/vikingyfy/sing-box/Makefile
 sed -i 's/1.15.0/1.15.0_alpha1/g' package/vikingyfy/luci-app-homeproxy/Makefile
 
 # 重新添加 luci-app-openclash
